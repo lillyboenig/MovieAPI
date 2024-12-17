7ee7d7a8cc3c78513fbda5a5ef15c15b767f288d
lillyboenig <lilly.boenig@web.de>
2024-12-17 14:29:51 +0200

    changing to Pooling

--- pg_server.js
+++ pg_server.js
@@ -1,18 +1,16 @@
 import express from 'express';
-import pg from 'pg';
+import {Pool} from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 
-const {Client} = pg;
-
 app.listen(3001, () => {
     console.log('The server is running!!');
 });
 
-const client = new Client({
+export default pgPool = new Pool({
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
